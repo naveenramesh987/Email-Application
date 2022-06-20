219da2341770bd570c590960f45b6b312e84a239
naveenramesh987 <44354099+naveenramesh987@users.noreply.github.com>
2022-06-20 11:59:35 -0700

    Update Application.java

--- Application.java
+++ Application.java
@@ -1,6 +1,6 @@
 public class Application {
     public static void main(String[] args) {
-        Email email = new Email("John", "Smith");
+        Email email = new Email("Naveen", "Ramesh");
         System.out.println(email.showInfo());
     }
 }
